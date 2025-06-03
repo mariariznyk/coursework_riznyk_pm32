0b09184cd32558554f3379d08333dc872b7a9c18
mariariznyk <144769926+mariariznyk@users.noreply.github.com>
2025-06-03 10:29:01 +0300

    Update CourseworkApplication.java

--- src/main/java/net/strbasic/coursework/CourseworkApplication.java
+++ src/main/java/net/strbasic/coursework/CourseworkApplication.java
@@ -3,12 +3,15 @@ package net.strbasic.coursework;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import java.util.TimeZone;
+
 
 @SpringBootApplication
 @EnableScheduling  
 public class CourseworkApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Europe/Kyiv"));
         SpringApplication.run(CourseworkApplication.class, args);
     }
 }
